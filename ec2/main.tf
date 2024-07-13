28565346fa428e439d1a90c72daa1cc13bbcb786
kei_creativelife <kenken19920616@gmail.com>
2024-07-13 23:31:46 +0900

    ec2ファイルを修正

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,23 +1,3 @@
-# 2.EC2を起動
-terraform {
-  # Terraformがリソースを作成や更新削除などするためのAPIみたいなもの
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  # Terraformのver.を指定
-  required_version = ">= 1.2.0"
-}
-
-# 使用するproviderを定義するブロックq
-provider "aws" {
-  region     = "ap-northeast-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 resource "aws_instance" "app_server" {
   # MEMO: RegionによりAMI IDが異なる
   ami           = var.ami_id
--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
-module "ec2" {
-  source            = "./ec2"
+terraform {
+  # Terraformがリソースを作成や更新削除などするためのAPIみたいなもの
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  # Terraformのver.を指定
+  required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# module "vpc" {
+#   source = "./vpc"
+# }
+
+# module "ec2" {
+#   source = "./ec2"
+# }
