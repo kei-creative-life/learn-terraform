b77d27022c8aaa6c7bb6883450ba227a4bc2e85a
kei_creativelife <kenken19920616@gmail.com>
2024-07-14 00:11:26 +0900

    ルートテーブルを作成してインターネット接続できるようにする

--- vpc/main.tf
+++ vpc/main.tf
@@ -24,3 +24,22 @@ resource "aws_subnet" "terraform_dev_web_subnet" {
     Name = "terraform-dev-web-subnet"
   }
 }
+
+# ルートテーブル
+resource "aws_route_table" "terraform_dev_route_table" {
+  vpc_id = aws_vpc.terraform_dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform_dev_internet_gateway.id
+  }
+
+  tags = {
+    Name = "terraform-dev-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.terraform_dev_web_subnet.id
+  route_table_id = aws_route_table.terraform_dev_route_table.id
+}
